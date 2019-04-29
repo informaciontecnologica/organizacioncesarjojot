4d9ef828d6f66cd93bb7763a9d179b2e140eee15
daniel <jorge.daniel.castro.formosa@gmail.com>
2019-04-29 07:49:24 -0300

    aa

--- index.php
+++ index.php
@@ -4,14 +4,129 @@ To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
 and open the template in the editor.
 -->
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Organización Cesar Jojot - Bienvenidos</title>
+        <meta name="description" content="Aseguramos tu tranquilidad y la de los tuyos" /><meta name="keywords" content="Cesar jojot seguros, jojot, seguros,tranquilidad, seguro de auto, seguro de hogar, hogar, auto, coche, moto, barco, casa, campo, salud, enfermedad, asegurar" /><link rel="canonical" href="https://www.organizacioncesarjojot.com.ar" /><meta name="robots" content="index, follow" /><link rel="alternate" hreflang="es" href="https://organizacioncesarjojot.com.ar" /><link rel="alternate" hreflang="en" href="https://organizacioncesarjojot.com.ar" />
+
+
+        <meta property="og:locale" content="es" /><meta property="og:title" content="organizacion cesar jojot - Bienvenidos" /><meta property="og:type" content="website" /><meta property="og:url" content="https://organizacioncesarjojot.com.ar" /><meta property="og:site_name" content="Sancor Seguros Argentina" /><meta property="og:description" content="Brindamos una amplia variedad de seguros para las personas y su patrimonio: autos, hogar, salud, empresas, comercios y campos." /><meta property="og:image" />
+
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+
+
+
+
+        AGRO
+        
+        <nav>
+            SEGUROS 
+                INDIVIDUOS
+                    HOGAR
+                    VEHICULOS
+                    VIDA Y SALUD
+                    ACCIDENTES PERSONALES
+                    
+                EMPRESAS
+                    GRANDES EMPRESAS
+                    COMERCIOS Y PYMES
+                    INSTITUCIONES EDUCATIVAS
+                    EMPLEADOS
+                AGRO
+                    CULTIVOS
+                    GANADO
+                    PATRIMONIO
+                    EMPLEADOS
+            
+            SINIESTROS 
+                INDIVIDUOS
+                    HOGAR
+                    HOGAR Y OTROS BIENES
+                    VIDA Y SALUD
+                    ACCIDENTES PERSONALES
+                EMPRESAS
+                    VEHÍCULOS EMPRESAS
+                    GRANDES EMPRESAS, COMERCIOS Y PYMES
+                    EMPLEADOS
+                    INSTITUCIONES EDUCATIVAS
+                AGRO
+                    CULTIVOS
+                    GANADO
+                    EMPLEADOS
+                    PATRIMONIO
+            
+            ATENCIÓN AL CLIENTE 
+            PRODUCTORES Y PROVEEDORES 
+            INSTITUCIONAL 
+            CONTACTO 
+            🔍</nav>
+        <section>
+            Mejor seguro auto 
+
+            INDIVIDUOS
+            Te ofrecemos las coberturas más completas para proteger tu vida, tu salud, tu familia y tus bienes personales.
+
+            HOGARHOGAR
+            Cada hogar es diferente y merece un seguro único y a medida. Por ello te ofrecemos seguridad y respaldo ante los imprevistos que sufra tu propiedad.
+
+            VEHÍCULOSVEHÍCULOS
+            Conocemos la comodidad e independencia personal que generan los automóviles. Por ello te garantizamos confianza y tranquilidad.
+
+            VIDA Y SALUDVIDA Y SALUD
+            Sabemos lo importante que es contar con una cobertura apropiada para afrontar los momentos más difíciles. Por ello te brindamos respaldo y seguridad.
+
+            ACCIDENTES PERSONALESACCIDENTES PERSONALES
+            Entendemos la importancia que es sentirse cubierto y protegido durante la jornada laboral. Garantizamos seguridad y respaldo.
+
+            EMPRESAS
+            Te ofrecemos una cobertura total que garantice el amparo del patrimonio de tu empresa, protegiendo los bienes ante los riesgos que puedan afectarlos.
+
+            GRANDES EMPRESASGRANDES EMPRESAS
+            Te brindamos una amplia gama de coberturas acordes para la protección total de los bienes muebles e inmuebles que conforman tu empresa.
+
+            COMERCIOS Y PYMESCOMERCIOS Y PYMES
+            Te ofrecemos seguros que cuenten con variedad, tranquilidad, respaldo y confianza para cubrir todas las necesidades que tu comercio requiera.
+
+            INSTITUCIONES EDUCATIVASINSTITUCIONES EDUCATIVAS
+            Te ofrecemos coberturas pensadas en el cuidado de las instalaciones educativas y en la protección física de los alumnos.
+
+            EMPLEADOSEMPLEADOS
+            Te ofrecemos coberturas aptas para la protección integral de los riesgos laborales que puedan ocasionarles tanto a empleados como empresarios.
+
+            AGRO
+            Brindamos la cobertura necesaria para proteger las contingencias a las que se exponen tus cultivos, tu ganado y tu capital más preciado.
+
+            CULTIVOSCULTIVOS
+            Conocemos el sacrificio que implica mantener viva la producción de tu campo. Brindamos protección y respaldo ante los daños que puedan perjudicarlos.
+
+            GANADOGANADO
+            Los animales de tu campo requieren atención y cuidado. Te ofrecemos la mejor protección y atención que merecés.
+
+            PATRIMONIOPATRIMONIO
+            Entendemos que tu tarea implica una gran inversión. Por ello protegemos los bienes de tu campo para que puedas ocuparte únicamente de la producción.
+
+            EMPLEADOS
+        </section>
+        <section>Mapa de Oficinas
+            Encontrá la oficina más cercana.
+        </section>
+        <footer>
+            N° de Inscripción de SSN xxxx
+
+            Atención al asegurado : 03704 
+
+
+            Organismo de control
+            www.argentina.gob.ar/ssn
+
+            La entidad aseguradora dispone de un Servicio de Atención al Asegurado que atenderá las consultas y reclamos que presenten los tomadores de seguros, asegurados, beneficiarios y/o derechohabientes.
+            El Servicio de Atención al Asegurado está integrado por:
+            RESPONSABLE: 
+            SUPLENTE: 
+            En caso de que el reclamo no haya sido resuelto o haya sido desestimado, total o parcialmente, o que haya sido denegada su admisión, podrá comunicarse con la Superintendencia de Seguros de la Nación por teléfono al 0800-666-8400, correo electrónico a denuncias@ssn.gob.ar o formulario web a través de www.argentina.gob.ar/ssn
+            Data Fiscal  Protección de Datos Personales
+        </footer>
     </body>
 </html>
